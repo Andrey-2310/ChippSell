31c3e6a2bf5d7b6e2b317c4e9d0fd5c3f675a3af
Andrey-2310 <andreyredkovskiy@mail.ru>
2017-07-05 12:32:41 +0300

    Model

---
+++ Chip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chipper.Models
+{
+    public class Chip
+    {
+        public string Name { get; set; }
+        public string Reference { get; set; }
+        public string ImageReference { get; set; }
+        public string Price { get; set; }
+        public string Availability { get; set; }
+    }
+}
